8819482db20feb1d32b0a53008fac3dd643e211e
gjl <gjl@gjldeMacBook-Pro.local>
2018-09-30 16:01:31 +0800

    update animation

--- app/src/main/java/com/moxun/tagcloud/MainActivity.java
+++ app/src/main/java/com/moxun/tagcloud/MainActivity.java
@@ -1,7 +1,6 @@
 package com.moxun.tagcloud;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.widget.Toast;
 
@@ -16,7 +16,7 @@ import com.moxun.tagcloudlib.view.TagCloudView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String TAG=MainActivity.class.getSimpleName();
+    private final String TAG = MainActivity.class.getSimpleName();
     private TagCloudView tagCloudView;
     private TextTagsAdapter textTagsAdapter;
     private ViewTagsAdapter viewTagsAdapter;
@@ -38,33 +38,13 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(ViewGroup parent, final View view, int position) {
 
 
-                Toast.makeText(getBaseContext(),"postion:"+position+" clicked",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(), "postion:" + position + " clicked", Toast.LENGTH_SHORT).show();
 
 
                 tagCloudView.stop();
-                final float originScaleX=view.getScaleX();
-                final float originScaleY=view.getScaleY();
-
-                final float originAlpha=view.getAlpha();
-
-                AnimationSet animationSet=new AnimationSet(true);
-                ScaleAnimation scaleUpAnimation=new ScaleAnimation(originScaleX,originScaleX,originScaleY,originScaleY, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-
-                Log.e(TAG,"originScaleX:"+originScaleX+"  originScaleY:"+originScaleY);
-                scaleUpAnimation.setDuration(300);
-                scaleUpAnimation.setFillEnabled(false);
-                animationSet.addAnimation(scaleUpAnimation);
 
 
-                ScaleAnimation scaleDownAnimation=new ScaleAnimation(1.5f,originScaleX,1.5f,originScaleY,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-
-                scaleDownAnimation.setFillAfter(true);
-                scaleDownAnimation.setDuration(150);
-
-                scaleDownAnimation.setStartOffset(150);
-                //animationSet.addAnimation(scaleDownAnimation);
-
-                animationSet.setAnimationListener(new Animation.AnimationListener() {
+                startViewAnimation(view, new Animation.AnimationListener() {
                     @Override
                     public void onAnimationStart(Animation animation) {
 
@@ -75,6 +55,15 @@ public class MainActivity extends AppCompatActivity {
 
 
                         view.clearAnimation();
+
+                        view.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                tagCloudView.start();
+
+                            }
+                        }, 300);
                     }
 
                     @Override
@@ -82,17 +71,6 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
-                view.startAnimation(animationSet);
-
-
-                view.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        tagCloudView.start();
-
-                    }
-                },200);
 
 
             }
@@ -125,5 +103,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, FragmentTestActivity.class));
             }
         });
+    }
+
+    private void startViewAnimation(View view, Animation.AnimationListener animationListener) {
+        final float originScaleX = view.getScaleX();
+        final float originScaleY = view.getScaleY();
+
+        final float originAlpha = view.getAlpha();
+
+        AnimationSet animationSet = new AnimationSet(false);
+        ScaleAnimation scaleUpAnimation = new ScaleAnimation(1f, 1.5f, 1f, 1.5f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleUpAnimation.setDuration(500);
+        scaleUpAnimation.setInterpolator(new LinearInterpolator());
+        scaleUpAnimation.setRepeatCount(1);
+        scaleUpAnimation.setRepeatMode(Animation.REVERSE);
+        animationSet.addAnimation(scaleUpAnimation);
+
+
+        Log.e(TAG, "originScaleX:" + originScaleX + "  originScaleY:" + originScaleY);
+
+
+
+        animationSet.setAnimationListener(animationListener);
+        view.startAnimation(animationSet);
+
+
     }
 }
